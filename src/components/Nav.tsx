03e712340cf1ad5530b257b6a04a8e0ad73da717
yude <i@yude.jp>
2025-04-13 19:09:04 +0900

    Mantela Editor は、

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ export default function Nav() {
     return (
         <div className="mt-2">
             <p className="text-3xl">☎️ Mantela Editor</p>
-            <p>Mantela Viewer は、<a href="https://github.com/tkytel/mantela">Mantela</a> 記述を行うためのヘルパー UI を提供します。</p>
+            <p>Mantela Editor は、<a href="https://github.com/tkytel/mantela">Mantela</a> 記述を行うためのヘルパー UI を提供します。</p>
         </div>
     )
 }
