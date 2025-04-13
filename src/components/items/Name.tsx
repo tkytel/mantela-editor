8271e84fc3045940d97572ebc2bc85cd44cdd592
yude <i@yude.jp>
2025-04-13 15:06:35 +0900

    Add support for identifier, name, preferredPrefix

---
+++ src/components/items/Name.tsx
@@ -0,0 +1,30 @@
+import { useImmerAtom } from "jotai-immer";
+import { BodyAtom } from "../../helpers/Jotai";
+
+export default function Name() {
+    const [json, setJson] = useImmerAtom(BodyAtom);
+    
+    return (
+        <div className="mb-5">
+            <label
+                htmlFor="aboutMe.identifier"
+                className="block mb-2 text-sm font-medium text-gray-900"
+            >
+                名前
+            </label>
+            <div className="relative w-full">
+                <input
+                    type="text"
+                    id="aboutMe.identifier"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    onChange={
+                        (e) => setJson(draft => {
+                            draft.data.aboutMe.name = e.target.value
+                        })
+                    }
+                    value={json.data.aboutMe.name}
+                />
+            </div>
+        </div>
+    )
+}
---
+++ src/components/items/PreferredPrefix.tsx
@@ -0,0 +1,44 @@
+import { useImmerAtom } from "jotai-immer";
+import { BodyAtom } from "../../helpers/Jotai";
+import Creatable from 'react-select/creatable';
+import { MultiValue } from 'react-select';
+
+type Option = { value: string; label: string; }
+
+export default function PreferredPrefix() {
+    const [json, setJson] = useImmerAtom(BodyAtom);
+
+    // JSON 側から更新された prefix を、react-select/creatable でハンドルできる型に変換する
+    const selectedPrefixes: Option[] = (json.data.aboutMe.preferredPrefix || []).map((value: string) => ({
+        value,
+        label: value,
+    }));
+
+    // UI 側から更新された prefix を、react-select/creatable の要素を排除して JSON 側に反映させる
+    const handleChange = (selected: MultiValue<Option>) => {
+        const values = selected ? selected.map((opt: any) => opt.value) : [];
+        setJson((draft) => {
+            draft.data.aboutMe.preferredPrefix = values;
+        })
+    }
+    
+    return (
+        <div className="mb-5">
+            <label
+                htmlFor="aboutMe.identifier"
+                className="block mb-2 text-sm font-medium text-gray-900"
+            >
+                好ましいプレフィックス
+            </label>
+            <div className="relative w-full">
+                <Creatable
+                    isClearable
+                    isMulti
+                    onChange={handleChange}
+                    value={selectedPrefixes}
+                    placeholder="プレフィックスを入力して、リターンキーを押してください..."
+                />
+            </div>
+        </div>
+    )
+}
