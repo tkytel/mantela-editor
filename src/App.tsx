e6df9b447219c9c21c8aba6cc7f1ae42d0b4562f
yude <i@yude.jp>
2025-04-13 22:23:53 +0900

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Nav from "./components/Nav";
 import Editor from "./components/Editor";
 import { Provider } from "jotai";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <Provider>
       <div className="container mx-auto">
         <Nav />
         <Editor />
+        <Footer />
       </div>
     </Provider>
   )
