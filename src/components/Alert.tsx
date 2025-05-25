e6d0de0a152189ede36c9b53874a309ee6f193ca
yude <i@yude.jp>
2025-05-25 15:42:34 +0900

    ddd

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -7,7 +7,7 @@ export default function Alert() {
     return (
         <>
         { Object.entries(alerts).length != 0 &&
-            <div id="top-alert-additional-content-5" className="absolute top-0 left-1/4 transform -translate-x-1/2 p-4 border border-gray-300 rounded-lg bg-gray-50 max-w-2xl" role="alert">
+            <div id="top-alert-additional-content-5" className="absolute top-0 left-1/4 transform -translate-x-1/2 p-4 border border-gray-300 rounded-lg bg-gray-50 max-w-2xl opacity-[.85]" role="alert">
                 <div className="flex items-center">
                     <svg className="shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
--- src/components/items/Extension.tsx
+++ src/components/items/Extension.tsx
@@ -55,12 +55,32 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
     }
 
     const [isSetCoord, setIsSetCoord] = useState(false);
+    const [identifier, setIdentifier] = useState("");
     const [latitude, setLatitude] = useState("");
     const [longitude, setLongitude] = useState("");
     const [altitude, setAltitude] = useState("");
     const [accuracy, setAccuracy] = useState("");
     const [altitudeAccuracy, setAltitudeAccuracy] = useState("");
     const [_, setAlerts] = useImmerAtom(AlertAtom);
+
+    useEffect(() => {
+        if (identifier !== "" && identifier.match("^[\\x21-\\x7E]+$")) {
+            setJson(draft => {
+                draft.data.extensions[idx].identifier = identifier
+            })
+            setAlerts(draft => {
+                delete draft.alerts["extensions[" + idx + "].identifier"]
+            })
+        } else if (identifier == "") {
+            setAlerts(draft => {
+                delete draft.alerts["extensions[" + idx + "].identifier"]
+            })
+        } else {
+            setAlerts(draft => {
+                draft.alerts["extensions[" + idx + "].identifier"] = "局の識別子は半角英数字と記号 (!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~) のみを使用してください。"
+            })
+        }
+    }, [identifier])
 
     useEffect(() => {
         const num = Number(latitude)
@@ -71,15 +91,15 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
                 }
             })
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.latitude"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.latitude"]
             })
         } else if (latitude == ""){
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.latitude"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.latitude"]
             })
         } else {
             setAlerts(draft => {
-                draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.latitude"] = "緯度は -90 から 90 の間の数値である必要があります。"
+                draft.alerts["extensions[" + idx + "].geolocationCoordinates.latitude"] = "緯度は -90 から 90 の間の数値である必要があります。"
             })
         }
     }, [latitude])
@@ -93,15 +113,15 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
                 }
             })
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.longitude"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.longitude"]
             })
         } else if (longitude == ""){
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.longitude"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.longitude"]
             })
         } else {
             setAlerts(draft => {
-                draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.longitude"] = "経度は -180 から 180 の間の数値である必要があります。"
+                draft.alerts["extensions[" + idx + "].geolocationCoordinates.longitude"] = "経度は -180 から 180 の間の数値である必要があります。"
             })
         }
     }, [longitude])
@@ -115,15 +135,15 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
                 }
             })
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.altitude"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.altitude"]
             })
         } else if (altitude == ""){
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.altitude"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.altitude"]
             })
         } else {
             setAlerts(draft => {
-                draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.altitude"] = "高度は 0 以上の数値でなければなりません。"
+                draft.alerts["extensions[" + idx + "].geolocationCoordinates.altitude"] = "高度は 0 以上の数値でなければなりません。"
             })
         }
     }, [altitude])
@@ -137,15 +157,15 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
                 }
             })
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.accuracy"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.accuracy"]
             })
         } else if (accuracy == ""){
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.accuracy"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.accuracy"]
             })
         } else {
             setAlerts(draft => {
-                draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.accuracy"] = "精度は 0 以上の数値でなければなりません。"
+                draft.alerts["extensions[" + idx + "].geolocationCoordinates.accuracy"] = "精度は 0 以上の数値でなければなりません。"
             })
         }
     }, [accuracy])
@@ -159,15 +179,15 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
                 }
             })
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.altitudeAccuracy"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.altitudeAccuracy"]
             })
         } else if (altitudeAccuracy == ""){
             setAlerts(draft => {
-                delete draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.altitudeAccuracy"]
+                delete draft.alerts["extensions[" + idx + "].geolocationCoordinates.altitudeAccuracy"]
             })
         } else {
             setAlerts(draft => {
-                draft.alerts["extensions.extension[" + idx + "].geolocationCoordinates.altitudeAccuracy"] = "高度の精度は 0 以上の数値でなければなりません。"
+                draft.alerts["extensions[" + idx + "].geolocationCoordinates.altitudeAccuracy"] = "高度の精度は 0 以上の数値でなければなりません。"
             })
         }
     }, [altitudeAccuracy])
@@ -321,21 +341,13 @@ export default function Extension({ extension, idx }: {extension: MantelaExtensi
                         type="text"
                         id={"extensions[" + idx + "].identifier"}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        onChange={
-                            (e) => setJson(draft => {
-                                draft.data.extensions[idx].identifier = e.target.value
-                            })
-                        }
-                        value={json.data.extensions[idx].identifier}
+                        onChange={(e) => setIdentifier(e.target.value)}
+                        value={identifier}
                     />
                     <button
                         type="button"
                         className="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                        onClick={() => {
-                            setJson(draft => {
-                                draft.data.extensions[idx].identifier = generateExtensionIdentifier();
-                            });
-                        }}
+                        onClick={() => {setIdentifier(generateExtensionIdentifier())}}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4" fill="currentColor">
                             <path stroke="currentColor" d="M105.1 202.6c7.7-21.8 20.2-42.3 37.8-59.8c62.5-62.5 163.8-62.5 226.3 0L386.3 160 352 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l111.5 0c0 0 0 0 0 0l.4 0c17.7 0 32-14.3 32-32l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 35.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5zM39 289.3c-5 1.5-9.8 4.2-13.7 8.2c-4 4-6.7 8.8-8.1 14c-.3 1.2-.6 2.5-.8 3.8c-.3 1.7-.4 3.4-.4 5.1L16 432c0 17.7 14.3 32 32 32s32-14.3 32-32l0-35.1 17.6 17.5c0 0 0 0 0 0c87.5 87.4 229.3 87.4 316.7 0c24.4-24.4 42.1-53.1 52.9-83.8c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.5 62.5-163.8 62.5-226.3 0l-.1-.1L125.6 352l34.4 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L48.4 288c-1.6 0-3.2 .1-4.8 .3s-3.1 .5-4.6 1z"/>
