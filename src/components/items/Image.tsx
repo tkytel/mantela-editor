3abdb7d0ab298286c59024137e668fbed1686ef3
eggplants <w10776e8w@yahoo.co.jp>
2025-08-31 23:09:51 +0900

    feat: add input form of aboutMe.image

---
+++ src/components/items/Image.tsx
@@ -0,0 +1,30 @@
+import { useImmerAtom } from "jotai-immer";
+import { BodyAtom } from "../../helpers/Jotai";
+
+export default function Image() {
+    const [json, setJson] = useImmerAtom(BodyAtom);
+    
+    return (
+        <div className="mb-5">
+            <label
+                htmlFor="aboutMe.image"
+                className="block mb-2 text-sm font-medium text-gray-900"
+            >
+                交換局に関連する画像の URL
+            </label>
+            <div className="relative w-full">
+                <input
+                    type="url"
+                    id="aboutMe.image"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    onChange={
+                        (e) => setJson(draft => {
+                            draft.data.aboutMe.image = e.target.value
+                        })
+                    }
+                    value={json.data.aboutMe.image ?? ""}
+                />
+            </div>
+        </div>
+    )
+}
