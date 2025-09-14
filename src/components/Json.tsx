c7ec4a5058563019b5fbda2cc07d9beb5bb3be2c
haruna <w10776e8w@yahoo.co.jp>
2025-09-15 08:47:29 +0900

    Remove error details from JSON editor (#48)
    * chore: remove useless script
    
    * feat: remove error details from json editor

--- src/components/Json.tsx
+++ src/components/Json.tsx
@@ -15,6 +15,7 @@ import { hoverTooltip } from "@codemirror/view";
 import { json as jsonLang, jsonLanguage, jsonParseLinter } from "@codemirror/lang-json";
 import { MantelaSchema } from "../types/mantela";
 import { BodyAtom, defaultMantelaSchemaUrl } from "../helpers/Jotai";
+import { Icon } from "./commons";
 
 // JSONSchema7
 type Schema = NonNullable<Parameters<typeof updateSchema>[1]>;
@@ -94,27 +95,11 @@ export default function Json() {
 			{parseErr !== "" && (
 				<div className="px-4 pt-3 border border-gray-300 rounded-lg bg-gray-50 mb-2" role="alert">
 					<div className="flex items-center">
-						<svg
-							aria-hidden="true"
-							className="shrink-0 w-4 h-4 me-2"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-						</svg>
+						<Icon variant="info" />
 						<span className="sr-only">Info</span>
-						<h3 className="text-lg font-medium text-gray-800">不正な mantela.json です</h3>
+						<h3 className="text-lg font-medium text-gray-800">不正な mantela.json です！</h3>
 					</div>
 					<div className="mt-2 mb-4 text-sm text-gray-800">
-						<div className="block p-3 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
-							{parseErr}
-						</div>
-						<h2 className="mb-2 text-base text-gray-900 mt-3">修正のためのヒント:</h2>
-						<ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside ml-2">
-							<li>必須なフィールドが存在していること</li>
-							<li>JSON として正しい文字列であること</li>
-						</ul>
 						<p className="mt-2">この状態で左側の UI を操作すると、変更が失われることがある点に留意してください。</p>
 					</div>
 				</div>
